7a0689d932d4350ee01d05394cf5d80f1cd01658
Yasmeen <yasmeenmas@gmx.de>
2023-07-19 11:50:13 +0200

    Transponierung: 1. Variante trim()

--- script_01.js
+++ script_01.js
@@ -81,11 +81,11 @@ Solange die Variable existiert, bleibt dieser erhalten
 hier: Verkettung eines Strings // Transponierung
 */
 
-let str = ""; // Anfangswert
-for (let i = 0; i < 4; i++) {
-   str += "Test";
-   output("inhalt von str: " + str);
-}
+// let str = ""; // Anfangswert
+// for (let i = 0; i < 4; i++) {
+//    str += "Test";
+//    output("inhalt von str: " + str);
+// }
 
 
 
@@ -135,15 +135,19 @@ function getSentenceArr(arr) {
 /*** 01b. Funktionalität mit Array 2  */
 // Transponierung:  untereinander ---> nebeneinander
 // Helge Schneider: Anananandereihung ...
-// output(getSentenceArr2(["Ich","bin","die","coole","Maxine","Mützerich"]));
+output(getSentenceArr2(["Ich","bin","die","coole","Maxine","Mützerich"]));
 function getSentenceArr2(arr) {
     const GAP = " ";
     const PUNCT = ".";
-    let str;
+    let str = ""; // Anfangswert
+
 
     for (let i = 0; i < arr.length; i++) {
-    output(arr[i]);  
+        str += arr[i] + GAP;
+       
     }
+    // 1. Variante trim()
+    str = str.trim()+ PUNCT;
 
     return str;
 }
